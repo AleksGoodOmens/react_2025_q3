153c9e52be932233272c7a5982699c37e95808bc
Aleks Gomeniuk <agsfinks@gmail.com>
2025-07-09 15:01:54 +0100

    feat: add aliasies to project

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import App from './App.tsx';
+import Home from '@pages/home/Home';
 
 const root = document.getElementById('root');
 
 if (root)
   createRoot(root).render(
     <StrictMode>
-      <App />
+      <Home />
     </StrictMode>
   );
