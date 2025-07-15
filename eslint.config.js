c17225de076f317ed6fe69e8aa26671607a53233
Aleks Gomeniuk <agsfinks@gmail.com>
2025-07-15 18:48:27 +0100

    feat: add test for button

--- eslint.config.js
+++ eslint.config.js
@@ -8,6 +8,7 @@ import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
+	
   { ignores: ['dist'] },
   {
     extends: [
---
+++ src/components/button/Button.test.tsx
@@ -0,0 +1,14 @@
+import { render, screen } from '@testing-library/react';
+import { describe, expect, it } from 'vitest';
+
+import Button from './Button';
+
+const mockButtonChildren = 'test button';
+
+describe('button test', () => {
+  it('render button with children', () => {
+    render(<Button>{mockButtonChildren}</Button>);
+    const btn = screen.getByRole('button');
+    expect(btn).toBeInTheDocument();
+  });
+});
