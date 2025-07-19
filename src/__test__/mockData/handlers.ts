e13f57c7d92dfc45318c9bf9a0b35ae53b89636d
Aleks Gomeniuk <agsfinks@gmail.com>
2025-07-19 13:11:56 +0100

    refactor: update handlers url path

--- src/__test__/mockData/handlers.ts
+++ src/__test__/mockData/handlers.ts
@@ -2,22 +2,25 @@ import { mockCountries } from './countries.mock';
 import { BASE_API_URL } from '@/constants';
 import { http, HttpResponse } from 'msw';
 
-const url = new URL('all', BASE_API_URL);
-
-export const test_base_url = url.href;
-export const test_url_by_name =
-  'https://restcountries.com/v3.1/translation/test';
-export const test_url_by_non_existed =
-  'https://restcountries.com/v3.1/translation/non_existent_country';
+const NETWORK_DELAY = 10;
+export const test_base_url = `${BASE_API_URL}all`;
+export const test_url_by_name = `${BASE_API_URL}translation/test`;
+export const test_url_by_non_existed = `${BASE_API_URL}translation/non_existent_country`;
 export const handlers = [
-  http.get(test_base_url, () => {
+  http.get(test_base_url, async () => {
+    await new Promise((resolve) => setTimeout(resolve, NETWORK_DELAY));
+
     return HttpResponse.json(mockCountries);
   }),
 
-  http.get(test_url_by_name, () => {
+  http.get(test_url_by_name, async () => {
+    await new Promise((resolve) => setTimeout(resolve, NETWORK_DELAY));
+
     return HttpResponse.json(mockCountries[0]);
   }),
-  http.get(test_url_by_non_existed, () => {
+  http.get(test_url_by_non_existed, async () => {
+    await new Promise((resolve) => setTimeout(resolve, NETWORK_DELAY));
+
     return HttpResponse.json([]);
   }),
 ];
--- src/components/country-list/CountryList.test.tsx
+++ src/components/country-list/CountryList.test.tsx
@@ -1,5 +1,5 @@
 import { CountryList } from './CountryList';
-import { mockCountries, server } from '@/__test__';
+import { server } from '@/__test__';
 import { BASE_API_URL } from '@/constants';
 import { http, HttpResponse } from 'msw';
 import {
@@ -57,29 +57,15 @@ describe('CountryList', () => {
   describe('data loading', () => {
     it('should display countries', async () => {
       render(<CountryList />);
-      expect(await screen.findAllByRole('status')).toHaveLength(20);
 
       expect(await screen.findAllByRole('listitem')).toHaveLength(3);
     });
     it('should show skeletons during search and then results', async () => {
-      server.use(
-        http.get('https://restcountries.com/v3.1/all', async () => {
-          await new Promise((resolve) => setTimeout(resolve, 100)); // Задержка
-          return HttpResponse.json(mockCountries);
-        }),
-        http.get('https://restcountries.com/v3.1/translation*', async () => {
-          await new Promise((resolve) => setTimeout(resolve, 300)); // Задержка
-          return HttpResponse.json([mockCountries[0]]);
-        })
-      );
-
       render(<CountryList />);
 
       expect(screen.getAllByRole('status', { busy: true })).toHaveLength(20);
 
-      await waitFor(() => {
-        expect(screen.getAllByRole('listitem')).toHaveLength(3);
-      });
+      expect(await screen.findAllByRole('listitem')).toHaveLength(3);
 
       window.history.pushState({}, '', '?search=test');
       window.dispatchEvent(new CustomEvent('searchUpdated'));
@@ -87,49 +73,27 @@ describe('CountryList', () => {
         20
       );
 
-      await waitFor(() => {
-        expect(screen.getAllByRole('listitem')).toHaveLength(1);
-      });
+      expect(await screen.findAllByRole('listitem')).toHaveLength(1);
     });
   });
 
   describe('empty state', () => {
     it('should show "No countries found"', async () => {
-      server.use(
-        http.get('https://restcountries.com/v3.1/translation/*', () => {
-          return HttpResponse.json([]);
-        })
-      );
-
-      window.history.pushState({}, '', '?search=empty');
+      window.history.pushState({}, '', '?search=non_existent_country');
       render(<CountryList />);
 
-      await waitFor(
-        () => {
-          expect(screen.getByText(/no countries found/i)).toBeInTheDocument();
-        },
-        { timeout: 200 }
-      );
+      expect(
+        await screen.findByText(/no countries found/i)
+      ).toBeInTheDocument();
     });
   });
 
   describe('search', () => {
     it('should filter by URL param', async () => {
-      server.use(
-        http.get('https://restcountries.com/v3.1/translation/test', () => {
-          return HttpResponse.json([mockCountries[0]]);
-        })
-      );
-
       window.history.pushState({}, '', '?search=test');
       render(<CountryList />);
 
-      await waitFor(
-        () => {
-          expect(screen.getAllByRole('listitem')).toHaveLength(1);
-        },
-        { timeout: 200 }
-      );
+      expect(await screen.findAllByRole('listitem')).toHaveLength(1);
     });
   });
 });
