a2d577dad4afaeff986f55b52eda35fe75c2c29a
Aleks Gomeniuk <agsfinks@gmail.com>
2025-07-22 13:36:15 +0100

    refactor: convert country-item to functional component

--- src/components/country-item/CountryItem.tsx
+++ src/components/country-item/CountryItem.tsx
@@ -1,36 +1,37 @@
 import type { ICountry } from '@/interfaces';
-import { PureComponent, type ReactNode } from 'react';
 
-export class CountryItem extends PureComponent<ICountry> {
-  render(): ReactNode {
-    const { area, capital, flags, name } = this.props;
-
-    return (
-      <li className="flex items-center gap-2 border-b-2 p-1">
-        <img
-          width={75}
-          height={'100%'}
-          className="rounded-md object-cover"
-          src={flags.svg}
-          alt={flags.alt}
-        />
-        <div>
-          <p className="font-bold">{name.official}</p>
-          {capital ? (
-            <p className="font-bold">
-              Capital:
-              {capital.map((cap) => (
-                <span className="font-normal" key={cap}>
-                  {cap}
-                </span>
-              ))}
-            </p>
-          ) : null}
-          <p className="font-bold">
-            Area: <span className="font-normal">{area} km²</span>
-          </p>
-        </div>
-      </li>
-    );
-  }
+interface Props {
+  countryData: ICountry;
 }
+
+export const CountryItem = ({ countryData }: Props) => {
+  const { area, capital, flags, name } = countryData;
+
+  return (
+    <li className="flex items-center gap-2 border-b-2 p-1">
+      <img
+        width={75}
+        height={'100%'}
+        className="rounded-md object-cover"
+        src={flags.svg}
+        alt={flags.alt}
+      />
+      <div>
+        <p className="font-bold">{name.official}</p>
+        {capital ? (
+          <p className="font-bold">
+            Capital:
+            {capital.map((cap) => (
+              <span className="font-normal" key={cap}>
+                {cap}
+              </span>
+            ))}
+          </p>
+        ) : null}
+        <p className="font-bold">
+          Area: <span className="font-normal">{area} km²</span>
+        </p>
+      </div>
+    </li>
+  );
+};
