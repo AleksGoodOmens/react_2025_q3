6d34e5a74cdfd7cba1ac5c21cecfb7182303f103
Aleks Gomeniuk <agsfinks@gmail.com>
2025-07-22 14:59:22 +0100

    refactor: transfer search input to functional component with react memo

--- src/components/search-input/SearchInput.tsx
+++ src/components/search-input/SearchInput.tsx
@@ -1,43 +1,28 @@
-import {
-  Component,
-  type ChangeEvent,
-  type InputHTMLAttributes,
-  type ReactNode,
-} from 'react';
+import { memo, useCallback, useState, type ChangeEvent } from 'react';
 
-interface SearchInputProps extends InputHTMLAttributes<HTMLInputElement> {
+interface Props {
   label?: string;
 }
 
-export class SearchInput extends Component<SearchInputProps> {
-  state = {
-    value: '',
-  };
-  handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+export const SearchInput = memo(function SearchInput({ label }: Props) {
+  const [value, setValue] = useState(localStorage.getItem('search') || '');
+
+  const handleChange = useCallback((e: ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value;
 
-    this.setState({ value: value });
-  };
+    setValue(value);
+  }, []);
 
-  componentDidMount(): void {
-    const savedSearch = localStorage.getItem('search');
-    if (savedSearch) this.setState({ value: savedSearch });
-  }
-
-  render(): ReactNode {
-    const { label, ...inputProps } = this.props;
-    return (
-      <label className="flex w-full items-center gap-2 rounded-xl border-2 px-4 py-2 sm:col-span-5">
-        <h5 className="uppercase">{label || 'search'}</h5>
-        <input
-          className="w-full rounded-xl bg-amber-200 px-2 py-1"
-          type="search"
-          name={label || 'search'}
-          value={this.state.value}
-          onChange={this.handleChange}
-          {...inputProps}
-        />
-      </label>
-    );
-  }
-}
+  return (
+    <label className="flex w-full items-center gap-2 rounded-xl border-2 px-4 py-2 sm:col-span-5">
+      <h5 className="uppercase">{label || 'search'}</h5>
+      <input
+        className="w-full rounded-xl bg-amber-200 px-2 py-1"
+        type="search"
+        name={label || 'search'}
+        value={value}
+        onChange={handleChange}
+      />
+    </label>
+  );
+});
