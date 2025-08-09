9c06afff82ed43937657bfc36a962bcd17724971
Aleks Gomeniuk <agsfinks@gmail.com>
2025-08-09 20:01:05 +0100

    docs: add coverage folder to ignore eslint list

--- eslint.config.js
+++ eslint.config.js
@@ -9,7 +9,7 @@ import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
-  { ignores: ['dist'] },
+  { ignores: ['dist', 'coverage'] },
   ...pluginQuery.configs['flat/recommended'],
   {
     extends: [
