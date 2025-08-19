2f380444af9e43347dff1e5fded7e58c0d828e17
Aleks Gomeniuk <agsfinks@gmail.com>
2025-08-19 21:40:49 +0100

    feat: modal window

---
+++ src/components/modal-window/modal-window.tsx
@@ -0,0 +1,51 @@
+import { Button } from '../button/Button';
+import { useEffect, useRef, type PropsWithChildren } from 'react';
+import { createPortal } from 'react-dom';
+
+interface Props extends PropsWithChildren {
+  isOpen: boolean;
+  onClose: () => void;
+}
+
+export const ModalWindow = ({ isOpen, onClose, children }: Props) => {
+  const modalRef = useRef(null);
+
+  useEffect(() => {
+    const handleEscape = (e: KeyboardEvent) => {
+      if (e.key === 'esc') onClose();
+    };
+
+    if (isOpen) {
+      document.addEventListener('keydown', handleEscape);
+      document.body.style.overflow = 'hidden';
+    }
+
+    return () => {
+      document.removeEventListener('keydown', handleEscape);
+      document.body.style.overflow = 'unset';
+    };
+  }, [isOpen, onClose]);
+
+  if (!isOpen) return null;
+
+  return createPortal(
+    <div
+      className="absolute inset-2 top-0 left-0 grid h-full w-full items-center justify-center bg-black/40 backdrop-blur-sm"
+      onClick={onClose}
+    >
+      <div
+        className="rounded-2xl border-2 border-white bg-blue-400 p-2"
+        onClick={(e) => e.stopPropagation()}
+        ref={modalRef}
+        role="dialog"
+        aria-modal="true"
+      >
+        <Button variant="ghost" onClick={onClose} aria-label="Close modal">
+          ×
+        </Button>
+        {children}
+      </div>
+    </div>,
+    document.body
+  );
+};
--- src/pages/home/home-page.tsx
+++ src/pages/home/home-page.tsx
@@ -1,7 +1,47 @@
+import { useState } from 'react';
+
+import { Button, Heading, ThemeChanger } from '@/components';
+import { ModalWindow } from '@/components/modal-window/modal-window';
+
 export const HomePage = () => {
+  const [isUncontrolledFormOpen, setIsUncontrolledFormOpen] =
+    useState<boolean>(false);
+  const [isControlledFormOpen, setIsControlledFormOpen] =
+    useState<boolean>(false);
   return (
     <section>
-      <h1>Home page</h1>
+      <header className="flex justify-center">
+        <Heading variant="main" className="py-6 text-center">
+          Form by AmensGood
+        </Heading>
+        <ThemeChanger />
+      </header>
+      <div className="flex justify-center gap-4">
+        <Button
+          onClick={() => setIsUncontrolledFormOpen((prev) => !prev)}
+          variant="main"
+        >
+          show uncontrolled form
+        </Button>
+        <Button
+          variant="minor"
+          onClick={() => setIsControlledFormOpen((prev) => !prev)}
+        >
+          show controlled form
+        </Button>
+      </div>
+      <ModalWindow
+        onClose={() => setIsUncontrolledFormOpen((prev) => !prev)}
+        isOpen={isUncontrolledFormOpen}
+      >
+        <h1>uncontrolled form</h1>
+      </ModalWindow>
+      <ModalWindow
+        onClose={() => setIsControlledFormOpen((prev) => !prev)}
+        isOpen={isControlledFormOpen}
+      >
+        <h1>controlled form</h1>
+      </ModalWindow>
     </section>
   );
 };
