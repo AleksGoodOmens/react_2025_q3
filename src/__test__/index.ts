d593720d0f7b9ddcf4411485f58ce9d9119ad351
Aleks Gomeniuk <agsfinks@gmail.com>
2025-08-24 23:35:53 +0200

    feat: test coverage 46

--- src/__test__/index.ts
+++ src/__test__/index.ts
@@ -1,2 +1,6 @@
 import '@testing-library/jest-dom/vitest';
 import './mockData/mockLocalStorage.js';
+
+import userEvent from '@testing-library/user-event';
+
+export const user = userEvent.setup();
